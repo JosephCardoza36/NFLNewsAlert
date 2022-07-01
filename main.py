ecc2dec183cd668f68de49df80c73756b5eb844b
Joseph Cardoza <103144607+JosephCardoza36@users.noreply.github.com>
2022-07-01 13:07:02 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+
+NFL_URL = 'https://www.nfl.com/'
+MY_EMAIL = 'josephdeancardoza@gmail.com'
+MY_PASSWORD = 'xbnt kfsz eygv sbvz'
+MY_TEAM = 'Watson'
+
+nfl_page = requests.get(NFL_URL)
+soup = BeautifulSoup(nfl_page.text, 'html.parser')
+
+headlines = soup.find_all('span', class_='nfl-o-headlinestack__item-text')
+headline_list = [headline.text for headline in headlines]
+
+
+for headline in headline_list:
+    if MY_TEAM in headline:
+
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            result = connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg=f"Subject: BRONCOS HEADLINE\n\n{headline}\n{NFL_URL}news"
+                # I tried with the full message here, but wasn't working, I think that there is a limit.
+            )
+
